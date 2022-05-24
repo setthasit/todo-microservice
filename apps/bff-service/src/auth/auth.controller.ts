be0f0cac2138cf174e6ce2991c84013349efb543
Art.t <setthasit.th@gmail.com>
2022-05-24 23:24:04 +0700

    implement auth controller

--- apps/bff-service/src/auth/auth.controller.ts
+++ apps/bff-service/src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { auth } from '@todo-microservices/api-proto';
+import { firstValueFrom } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Inject(AuthService)
+  private service: AuthService
+
+  @Post("register")
+  async register(@Body() body: auth.RegisterRequest): Promise<auth.RegisterResponse> {
+    const response = await this.service.register(body)
+    return firstValueFrom(response)
+  }
+
+  @Post("signin")
+  async signIn(@Body() body: auth.SignInRequest): Promise<auth.SignInResponse> {
+    const response = await this.service.signIn(body)
+    return firstValueFrom(response)
+  }
+}
