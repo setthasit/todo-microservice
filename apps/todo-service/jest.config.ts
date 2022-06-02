b6e3aa063b0545d5814eafffbc9375a3209547dc
Art.t <setthasit.th@gmail.com>
2022-06-02 22:58:04 +0700

    change app name from todo to todo-service

--- apps/todo-service/jest.config.ts
+++ apps/todo-service/jest.config.ts
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/todo',
+  coverageDirectory: '../../coverage/apps/todo-service',
 };
